e7ac3fe23b119d29baa3a3460821d159ccd305a7
wan230114 <1170101471@qq.com>
2019-12-12 19:49:50 +0800

    1

--- gitee.sh
+++ gitee.sh
@@ -8,5 +8,5 @@ echo "***********"
 echo ">上传中..."
 read -p "Input this commit: " val echo $val
 git add -A
-git commit -a -m "`date "+%F  %H:%M:%S"` $val"
+git commit -a -m "$val"
 git push -u origin master        # 上传仓库到码云
